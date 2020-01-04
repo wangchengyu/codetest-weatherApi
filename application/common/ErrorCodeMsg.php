2e8736b1f8aacc4e7dc8572c5f14280e9c4b55b9
wangchengyu <wangchengyu@rongcapital.cn>
2020-01-05 03:30:05 +0800

    add a class for error defined

---
+++ application/common/ErrorCodeMsg.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\common;
+
+use think\Config;
+
+/**
+ * Class ErrorCodeMsg store code message array and some function
+ * @package app\common
+ */
+class ErrorCodeMsg
+{
+
+    const CODE_INVALID              = 0;
+    const CODE_OK                   = 200;
+    const CODE_AUTHENTICATED        = 401;
+    const CODE_NOT_FOUND            = 404;
+    const CODE_FORBIDDEN            = 403;
+    const CODE_SERVICE_ERROR        = 500;
+
+    /**
+     * @var store the map of code ==> message
+     */
+    protected static $store = null;
+
+    /**
+     * @param $code
+     * @return string
+     */
+    public static function getMessage($code) {
+
+        if (!self::$store) {
+            self::$store = Config::load('CodeMessage');
+        }
+
+        return isset(self::$store[$code]) ? self::$store[$code] : "";
+    }
+
+}
---
+++ application/extra/CodeMessage.php
@@ -0,0 +1,11 @@
+<?php
+//the map of Error Code and Message
+return [
+    0 => 'Invalid Error',
+    200 => 'OK',
+    400 => 'Bad Request',
+    401 => 'Authentication Failed',
+    403 => 'Forbidden Request',
+    404 => 'Resource Not Found',
+    500 => 'Internal Server Error',
+];
